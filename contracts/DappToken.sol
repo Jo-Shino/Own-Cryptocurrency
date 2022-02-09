f79e93e81511ff34020e364002e393ba52dbb0a7
Jo-Shino <joe000042@gmail.com>
2022-02-09 18:32:41 +0900

    balanceOf

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,10 +2,15 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract DappToken {
+  string public name = "DApp Token";
+  string public symbol = "DApp";
+  string public standard = "DApp Token v1.0";
   uint256 public totalSupply;
 
-  //function's name sholdn't be same contract's name 
-  constructor() public {
-      totalSupply = 1000000;
+  mapping(address => uint256) public balanceOf;
+
+  constructor(uint256 _initialSupply) {
+      balanceOf[msg.sender] = _initialSupply;
+      totalSupply = _initialSupply;
   }
 }
